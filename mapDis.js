0d996ff4add652aac941413c7637d4b686285dc2
ling_glb <1161420152@qq.com>
2019-02-21 16:09:37 +0800

    redux流程

---
+++ mapDis.js
@@ -0,0 +1,88 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+import { connect } from './react-redux'
+
+class ThemeSwitch extends Component {
+  static propTypes = {
+    themeColor: PropTypes.string,
+    onSwitchColor: PropTypes.func
+  }
+
+  handleSwitchColor (color) {
+    if (this.props.onSwitchColor) {
+      this.props.onSwitchColor(color)
+    }
+  }
+
+  render () {
+    return (
+      <div>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'red')}>Red</button>
+        <button
+          style={{ color: this.props.themeColor }}
+          onClick={this.handleSwitchColor.bind(this, 'blue')}>Blue</button>
+      </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {
+    themeColor: state.themeColor
+  }
+}
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onSwitchColor: (color) => {
+      dispatch({ type: 'CHANGE_COLOR', themeColor: color })
+    }
+  }
+}
+ThemeSwitch = connect(mapStateToProps, mapDispatchToProps)(ThemeSwitch)
+
+export default ThemeSwitch
+
+
+export const connect = (mapStateToProps, mapDispatchToProps) => (WrappedComponent) => {
+  class Connect extends Component {
+    static contextTypes = {
+      store: PropTypes.object
+    }
+
+    constructor () {
+      super()
+      this.state = {
+        allProps: {}
+      }
+    }
+
+    componentWillMount () {
+      const { store } = this.context
+      this._updateProps()
+      store.subscribe(() => this._updateProps())
+    }
+
+    _updateProps () {
+      const { store } = this.context
+      let stateProps = mapStateToProps
+        ? mapStateToProps(store.getState(), this.props)
+        : {} // 防止 mapStateToProps 没有传入
+      let dispatchProps = mapDispatchToProps
+        ? mapDispatchToProps(store.dispatch, this.props)
+        : {} // 防止 mapDispatchToProps 没有传入
+      this.setState({
+        allProps: {
+          ...stateProps,
+          ...dispatchProps,
+          ...this.props
+        }
+      })
+    }
+
+    render () {
+      return <WrappedComponent {...this.state.allProps} />
+    }
+  }
+  return Connect
